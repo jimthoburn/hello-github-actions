a2f7add1aff097321511146d38ce4936bbc267a5
Jim Thoburn <jim@jimthoburn.com>
2019-09-10 14:09:45 -0700

    Add authentication

--- .github/actions/hi.js
+++ .github/actions/hi.js
@@ -1,1 +1,19 @@
+const { Octokit } = require("@octokit/core");
+const { createActionAuth } = require("@octokit/auth-action");
+
+const auth = createActionAuth();
+const octokit = new Octokit({ auth });
+
+async function foo() {
+  const response = await octokit.request("GET /repos/:owner/:repo/contents/:path", {
+    owner: "jimthoburn",
+    repo: "hello-github-actions",
+    path: "README.md"
+  });
+
+  console.log(response.data);
+}
+
+foo();
+
 console.log("Hello world");
